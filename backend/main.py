d4989c5955645307e6cd681abb424a917c73a8d9
vicem-git <macellariv@gmail.com>
2025-07-04 14:09:46 +0200

    reorder

--- backend/main.py
+++ backend/main.py
@@ -3,3 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder="../frontend/dist", static_url_path="/")
 db = SQLAlchemy(app)
+
+
+@app.route("/")
+def index():
+    return app.send_static_file("index.html")
