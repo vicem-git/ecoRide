dd67cb530e533260069bc1628e86f209795212b8
vicem-git <macellariv@gmail.com>
2025-07-12 07:57:47 +0200

    gals

--- ecoride_flask/app/static/js/app.js
+++ ecoride_flask/app/static/js/app.js
@@ -1,4 +1,7 @@
 import '@/css/tw-output.css'
 import htmx from 'htmx.org';
-window.htmx = htmx;
+import { toggleHidden } from '@/js/modules/domUtils.js';
+// import { toggleTheme } from '@/js/toggle-theme.js';
 
+window.htmx = htmx;
+window.toggleHidden = toggleHidden;
